ef12b78f66c67af55a166b2326f7d27e3129fdaa
SaraLimaDosSantos <saralimadossantos2208@gmail.com>
2024-05-09 21:38:58 -0300

    Exercicio de JS

---
+++ JS/JS02-Variaveis/ExercicioCasa/main.js
@@ -0,0 +1,13 @@
+let nota1 = 10
+
+let nota2 = 9
+
+let nota3 = 1
+
+let nota4 = 5
+
+var soma = nota1 + nota2 + nota3 + nota4
+
+var media = soma / 4 
+
+console.log("A média do aluno com quatro notas é " + media)
---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,101 @@
+//Objeto console
+console.log("Hello World")
+
+//Variaveis 
+//quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+var _felippe =8
+_felippe = "Messi"
+//Variavel
+console.log(_felippe)
+
+let lestGo = 4 //não posso redclarar
+lestGo = 6
+console.log(lestGo)
+
+console.clear()
+
+// const
+const PI = 3.1415
+
+console.log(PI)
+
+let NAN =0 / 0
+
+console.log(NAN)
+
+let corinthians = "Melhor time, palmeiras sem mundial" // Variavel corinthians recebeu uma String 
+
+console.log("corinthians" + "melhor" + "palmeiras" + "sem" + "mundial")
+
+var numero3 =3
+var texto = "Eu tenho"
+var texto2 = "Ansiedade"
+
+console.log(texto + numero3 + texto2)
+
+console.clear();
+//Valores Booleanos
+
+var num1 = 1
+var num2 = 1
+
+console.log(num1 == num2);
+
+//Testes
+console.clear();
+
+//operadors
+var operador1 = 8;
+var operador2 = null
+
+console.log(operador1 * operador2); 
+
+var operador3 = "5"
+var operador4 = 1
+
+console.log(operador3 - operador4); // "5" - 1 = 4
+var operador5 = "5"
+var operador6 = 1
+
+console.log(operador5 + operador6); // "5" + 1 = "aqui acontece a concentaçao, resumindo o Java ve como texto e ira transformar isso"
+
+var operador7 = "five"
+var operador8 = 2
+
+console.log(operador7 * operador8); // "five" 
+
+//operadores aritméticos
+console.clear();
+
+var valor1 = 2
+var valor2 = 2
+var resultado = valor1 % valor2
+
+console.log(resultado);
+
+console.clear();
+
+var compara = 2 > 3 != 2 // false. Se aqui for false 
+
+var teste  = !compara //aqui sera verdadeiro
+
+//var compara = "2" === 2 (compara o valor e o tipo)
+
+//var compara = "2" == 2 (só esta comparando o valor)
+
+
+ console.log(compara);
+
+ console.log()
+
+ let valor3 = 4
+ let valor4 = 6
+ let resultado1 = valor3 > valor4 && valor3 == valor4
+
+//let resultado1 = valor3> valor4 // valor3 == valot4 
+ console.log(resultado1)
+
+var valor5 = "5"
+
+console.log(typeof valor5)
+
