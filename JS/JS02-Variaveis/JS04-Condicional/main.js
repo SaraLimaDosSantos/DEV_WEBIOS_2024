bfc6460f9e8dd03b5f29a083fc3a837d583a3942
SaraLimaDosSantos <saralimadossantos2208@gmail.com>
2024-05-14 21:42:17 -0300

    aula de condicional

---
+++ JS/JS02-Variaveis/JS04-Condicional/main.js
@@ -0,0 +1,35 @@
+var TodasDeCracha = true
+
+if( TodasDeCracha == true){
+    console.log("Todos recebem nota 10 em JS")
+}else{
+    console.log(`Todos os alunos vão receber 0 JS`)
+}
+
+
+console.clear()
+
+var num1 = 5
+var num2 = 10
+
+var comparar = 5==10
+
+if(comparar== true){
+    console.log("O número 5 é maior que o número 10")
+}else{
+    console.log("O número 5 é menor que o número 10")
+}
+
+console.clear()
+
+let mes = "JANEIRO"
+
+if(mes == "JANEIRO"){
+    console.log("é verão")
+}else if (mes == "JUNHO"){
+    console.log("È outono")
+}else if(mes == "OUTUBRO"){
+    console.log("È primavera")
+}else{
+    console.log("È inverno")
+}
