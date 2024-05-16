7368d0b2e47fb9dcd613a18d1b1b3c6f271f6a9f
SaraLimaDosSantos <saralimadossantos2208@gmail.com>
2024-05-15 21:47:26 -0300

    aula repeticao

--- JS/JS05-Repeticao/main.js
+++ JS/JS05-Repeticao/main.js
@@ -1,7 +1,22 @@
-var contador = 0
+// var contador = 0
 
-while(contador <= 10){
-   console.log( contador + "0 é menor que 10")
-   debugger
-   contador++;
+// while(contador <= 10){
+//    console.log( contador + " é menor que 10")
+//    debugger
+//    contador++;
+// }
+
+//Do-while
+// var contador = 0
+
+// do{
+//     debugger
+//    console.log( contador + " é menor que 10") 
+//    contador++; 
+// }while(contador < 10)
+
+var console = 10
+
+for(var contador = 10; contador <10; contador++){
+
 }
