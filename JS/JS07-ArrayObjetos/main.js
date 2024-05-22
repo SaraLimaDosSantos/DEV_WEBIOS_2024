4a3ae9b4c5bdb6f9258eff1318d336369e3826c3
SaraLimaDosSantos <saralimadossantos2208@gmail.com>
2024-05-22 19:41:37 -0300

    Aula de Array

---
+++ JS/JS07-ArrayObjetos/main.js
@@ -0,0 +1,16 @@
+//Objetos
+let pessoa = {
+    corDosOlhos:"Verde",
+    altura: "1.75",
+    corCabelo: "Preto",
+    nome: "Esther",
+    sobrenome:"Barreto",
+    hobbie: ["Futebol", "LOL", "Tocae guitarra"],
+    //Criar outro objeto
+    endereco{
+    rua:"Jao madeiro",
+    numero:"234",
+    cidade:"São Paulo",
+    estado:"SP"
+    }
+}
